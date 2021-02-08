e8777da3c7f7569bbdc6d2d91158e7d7d5aaddb4
unknown <shaikkarishama782@gmail.com>
2021-02-08 15:32:52 +0530

    modified

---
+++ index.js
@@ -0,0 +1,8 @@
+var card=document.createElement("div");
+card.setAttribute("id","card")
+CredentialsContainer.appendChild(card)
+var image=document.createElement("img");
+image.src="images.png"
+AbstractRange.alt="image"
+image.style.width="20%"
+card.appendChild(image)
